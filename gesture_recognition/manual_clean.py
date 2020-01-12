f2974b8d89b6dc87377fdcb742b836eff5d17450
Nicholas Liu <nicholas.j.liu@gmail.com>
2020-01-11 16:42:14 -0800

    remove old colored images and add manual cleaning script

---
+++ gesture_recognition/manual_clean.py
@@ -0,0 +1,33 @@
+import os
+import cv2
+import shutil
+
+'''
+move images that we do not want to be detected into the others directoru
+'''
+
+data_dir = './data/'
+paths = ['fist/', 'palm/', 'point_right/', 'point_left/', 'point_up/', 'point_down/']
+negatives = data_dir + 'other/'
+neg_i = len(os.listdir(negatives))
+
+for p in paths:
+    for img in os.listdir(data_dir + p):
+
+        img_path = data_dir + p + img
+        print(img_path)
+
+        frame = cv2.imread(img_path)
+        cv2.imshow('frame', frame)
+        
+        while True:
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord('n'):
+                break
+            elif key == ord('d'):
+                other_img_name = negatives + str(neg_i) + '.jpg'
+                shutil.move(img_path, other_img_name)
+                break
+
+
+
