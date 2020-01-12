e415f33b7cc4f27b2cf185da28c0f759782b755c
Eric Zhang <ezhang887@gmail.com>
2020-01-11 22:41:22 -0800

    Update inference

--- gesture_recognition/infer.py
+++ gesture_recognition/infer.py
@@ -4,13 +4,43 @@ import glob
 import tensorflow as tf
 import os
 import numpy as np
+import collections
 
-# params to set
 types = ["palm", "fist", "right", "left", "up", "down", "other"]
 
 bgr_low = (0, 150, 0)
 bgr_high = (50, 255, 50)
 image_buffer_directory = "imageBuffer/"
+
+ring_buf = collections.deque(maxlen=10)
+
+def valid_entry():
+    if not len(ring_buf) == 10:
+        return False
+    a = ring_buf[0]
+    for i in range(1, len(ring_buf)):
+        if ring_buf[i] != a:
+            return False
+    return True
+
+def int_to_type(i):
+    if i == 0:
+        return "fist"
+    elif i == 1:
+        return "palm"
+    elif i == 2:
+        return "up"
+    elif i == 3:
+        return "down"
+    elif i == 4:
+        return "left"
+    elif i == 5:
+        return "right"
+    elif i == 6:
+        return "other"
+    else:
+        print("WTF")
+        return None
 
 # entry point
 def main():
@@ -53,7 +83,11 @@ def main():
         r = np.expand_dims(resized.reshape(resized.shape + (1, )), axis=0)
 
         pred = model.predict_classes(r)
-        print(pred)
+        pred = int_to_type(pred)
+        ring_buf.append(pred)
+
+        if valid_entry():
+            print(ring_buf[1])
 
         cv2.imshow("mask", frame)
         cv2.imshow("resized", resized)
